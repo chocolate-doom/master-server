da3ab151690bf8344210cbf0c2040d4a019eb75b
Simon Howard <fraggle@gmail.com>
2010-12-26 21:32:24 +0000

    Split master server configuration options into a separate file, refactor so that the query socket can be bound to a specific address/port.
    Subversion-branch: /master
    Subversion-revision: 2229

---
+++ master_config.py
@@ -0,0 +1,43 @@
+#
+# Copyright(C) 2010 Simon Howard
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+#
+# Configuration file for master server.
+#
+
+# Filename of log file.
+
+LOG_FILE = "chocolate-master.log"
+
+# Servers must refresh themselves periodically.  If nothing happens
+# after this many seconds, remove them from the list.
+
+SERVER_TIMEOUT = 2 * 60 * 60  # 2 hours
+
+# How long is metadata valid before we force another query to refresh it?
+
+METADATA_REFRESH_TIME = 6 * 60 * 60 # 6 hours
+
+# Address and port to listen on.
+
+SERVER_ADDRESS = (None, 2342)
+
+# Address and port to bind query socket.
+
+QUERY_ADDRESS = None
+
